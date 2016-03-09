874f134c9827bf770535d304ade1eb9c2ee48cc1
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2016-03-09 22:24:32 +0300

    Implement more sophisticated moving to target

--- space.gd
+++ space.gd
@@ -26,17 +26,20 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		ship_position.y += space_ship_speed * delta
 		target = ship_position
-	if ship_position != target:
+	if distance_to_target() > 1.0:
 		if distance_to_target() > 2.0:
 			ship.rotate(ship.get_angle_to(target) + 3.1415)  # TODO hardcoded PI
-		if target.x > ship_position.x:
-			ship_position.x += space_ship_speed * delta
-		if target.x < ship_position.x:
-			ship_position.x -= space_ship_speed * delta
-		if target.y > ship_position.y:
-			ship_position.y += space_ship_speed * delta
-		if target.y < ship_position.y:
-			ship_position.y -= space_ship_speed * delta
+		var path = calculate_path_to_target()
+		ship_position.x += path.x * delta
+		ship_position.y += path.y * delta
+		#if target.x > ship_position.x:
+		#	ship_position.x += space_ship_speed * delta
+		#if target.x < ship_position.x:
+		#	ship_position.x -= space_ship_speed * delta
+		#if target.y > ship_position.y:
+		#	ship_position.y += space_ship_speed * delta
+		#if target.y < ship_position.y:
+		#	ship_position.y -= space_ship_speed * delta
 	get_node("ship").set_pos(ship_position)
 
 func _input(event):
@@ -44,6 +47,16 @@ func _input(event):
 		# var screen_position = get_pos()
 		#print(event.global_pos, " ", screen_position)
 		target = event.pos
+
+func calculate_path_to_target():
+	var s = get_node("ship").get_pos()
+	var t = target
+	var Dx = t.x - s.x
+	var Dy = t.y - s.y
+	var S = sqrt(Dx * Dx + Dy * Dy)
+	var f = space_ship_speed / S
+	return Vector2(f * Dx, f * Dy)
+	
 
 func distance_to_target():
 	var ship_pos = get_node("ship").get_pos()
